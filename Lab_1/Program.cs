aa8f0b897e1f63e4e08b6d374a416269f07e9f23
MEHT9IPA <filippanikin@mail.ru>
2019-10-11 18:13:11 +0300

    R

---
+++ Lab_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Input(ref double coeff)
+        {
+            string input = Console.ReadLine();
+            while (!double.TryParse(input, out coeff))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Некорректный ввод коэффициента, повторите попытку: ");
+                Console.ResetColor();
+                input = Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Аникин Филипп, ИУ5-33Б";
+
+            double A=0, B=0, C=0, discr;
+            Console.WriteLine("Введите коэффициенты биквадратного уравнения");
+            Console.Write("A = ");
+            Input(ref A);
+            Console.Write("B = ");
+            Input(ref B);
+            Console.Write("C = ");
+            Input(ref C);
+
+            Console.ReadKey();
+        }
+    }
+}
